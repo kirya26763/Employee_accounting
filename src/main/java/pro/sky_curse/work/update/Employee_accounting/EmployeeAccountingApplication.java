b8fac4b3d3ae6a7cd6fd5270291c34f0d307cba4
Arty <kirya26763@mail.ru>
2025-01-28 14:07:57 +0700

    init

---
+++ src/main/java/pro/sky_curse/work/update/Employee_accounting/EmployeeAccountingApplication.java
@@ -0,0 +1,13 @@
+package pro.sky_curse.work.update.Employee_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky_curse/work/update/Employee_accounting/EmployeeAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky_curse.work.update.Employee_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
